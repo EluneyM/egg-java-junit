d844a47995bb2185279a3b2f4e1128171fed6ea6
Matias <eluneymerillan@gmail.com>
2024-12-10 20:53:20 -0300

    test parametrizado calificaciones

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,44 @@ package src;
 import java.util.Scanner;
 
 public class Main {
-
     public static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("A");
+
+        Integer puntuacionIngresada = obtenerPuntuacion();
+
+        System.out.println(mensajePuntuacion(puntuacionIngresada));
     }
+
+    public static Integer obtenerPuntuacion() {
+        System.out.println("Ingrese una puntuación para calificar");
+        return sc.nextInt();
+    }
+
+    public static String mensajePuntuacion(Integer puntuacionIngresada) {
+        return "La clasificación correspondiente a la puntuación " +
+
+                puntuacionIngresada +
+
+                " corresponde a la letra " + obtenerNota(puntuacionIngresada);
+    }
+
+    public static String obtenerNota(Integer puntuacionIngresada) {
+        return switch (puntuacionIngresada) {
+            case 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100 -> "A";
+            case 80, 81, 82, 83, 84, 85, 86, 87, 88, 89 -> "B";
+            case 70, 71, 72, 73, 74, 75, 76, 77, 78, 79 -> "C";
+            case 60, 61, 62, 63, 64, 65, 66, 67, 68, 69 -> "D";
+            default -> {
+                if (puntuacionIngresada < 0 || puntuacionIngresada > 100) {
+                    yield ": Error - El número ingresado está fuera de rango válido.";
+
+                } else {
+                    yield "F";
+                }
+            }
+        };
+
+    }
+
 }
--- test/MainTest.java
+++ test/MainTest.java
@@ -9,10 +9,9 @@ import org.junit.Assert;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import src.Main;
 
@@ -22,18 +21,18 @@ public class MainTest {
     private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 
     @BeforeEach
-    public void setUp(){
+    public void setUp() {
         System.setOut(new PrintStream(outputStream));
     }
 
     @AfterEach
-    public void tearDown(){
+    public void tearDown() {
         System.setOut(printStream);
     }
 
     @DisplayName("Si la puntuación está entre 90 y 100, muestra 'A'")
     @ParameterizedTest
-    @CsvSource({ "'90\n'", "'95\n'", "'100\n'" })
+    @CsvFileSource(resources = "/csv/notesA.csv")
     void testMuestraA(String nota) {
 
         ByteArrayInputStream in = new ByteArrayInputStream(nota.getBytes());
@@ -44,6 +43,126 @@ public class MainTest {
 
         String salidaCapturada = outputStream.toString().trim();
 
-        Assert.assertEquals("A", salidaCapturada);
+        String result = "Ingrese una puntuación para calificar\n" + //
+                "La clasificación correspondiente a la puntuación " + nota.trim() + " corresponde a la letra A";
+
+        Assert.assertEquals(result, salidaCapturada);
     }
+
+    @DisplayName("Si la puntuación está entre 80 y 90, muestra 'B'")
+    @ParameterizedTest
+    @CsvSource({ "'80\n'", "'85\n'", "'89\n'" })
+    void testMuestraB(String nota) {
+
+        ByteArrayInputStream in = new ByteArrayInputStream(nota.getBytes());
+        System.setIn(in);
+        Main.sc = new Scanner(System.in);
+
+        Main.main(new String[1]);
+
+        String salidaCapturada = outputStream.toString().trim();
+
+        String result = "Ingrese una puntuación para calificar\n" + //
+                "La clasificación correspondiente a la puntuación " + nota.trim() + " corresponde a la letra B";
+
+        Assert.assertEquals(result, salidaCapturada);
+    }
+
+    @DisplayName("Si la puntuación está entre 80 y 90, muestra 'C'")
+    @ParameterizedTest
+    @CsvSource({ "'70\n'", "'75\n'", "'79\n'" })
+    void testMuestraC(String nota) {
+
+        ByteArrayInputStream in = new ByteArrayInputStream(nota.getBytes());
+        System.setIn(in);
+        Main.sc = new Scanner(System.in);
+
+        Main.main(new String[1]);
+
+        String salidaCapturada = outputStream.toString().trim();
+
+        String result = "Ingrese una puntuación para calificar\n" + //
+                "La clasificación correspondiente a la puntuación " + nota.trim() + " corresponde a la letra C";
+
+        Assert.assertEquals(result, salidaCapturada);
+    }
+
+    @DisplayName("Si la puntuación está entre 80 y 90, muestra 'B'")
+    @ParameterizedTest
+    @CsvSource({ "'60\n'", "'65\n'", "'69\n'" })
+    void testMuestraD(String nota) {
+
+        ByteArrayInputStream in = new ByteArrayInputStream(nota.getBytes());
+        System.setIn(in);
+        Main.sc = new Scanner(System.in);
+
+        Main.main(new String[1]);
+
+        String salidaCapturada = outputStream.toString().trim();
+
+        String result = "Ingrese una puntuación para calificar\n" + //
+                "La clasificación correspondiente a la puntuación " + nota.trim() + " corresponde a la letra D";
+
+        Assert.assertEquals(result, salidaCapturada);
+    }
+
+    @DisplayName("Si la puntuación está entre 80 y 90, muestra 'B'")
+    @ParameterizedTest
+    @CsvSource({ "'40\n'", "'1\n'", "'59\n'" })
+    void testMuestraF(String nota) {
+
+        ByteArrayInputStream in = new ByteArrayInputStream(nota.getBytes());
+        System.setIn(in);
+        Main.sc = new Scanner(System.in);
+
+        Main.main(new String[1]);
+
+        String salidaCapturada = outputStream.toString().trim();
+
+        String result = "Ingrese una puntuación para calificar\n" + //
+                "La clasificación correspondiente a la puntuación " + nota.trim() + " corresponde a la letra F";
+
+        Assert.assertEquals(result, salidaCapturada);
+    }
+
+    @DisplayName("Si la puntuación está entre 80 y 90, muestra 'B'")
+    @ParameterizedTest
+    @CsvSource({ "'101\n'", "'-1\n'", "'-100\n'" })
+    void testMuestraError(String nota) {
+
+        ByteArrayInputStream in = new ByteArrayInputStream(nota.getBytes());
+        System.setIn(in);
+        Main.sc = new Scanner(System.in);
+
+        Main.main(new String[1]);
+
+        String salidaCapturada = outputStream.toString().trim();
+
+        String result = "Ingrese una puntuación para calificar\n" + //
+                "La clasificación correspondiente a la puntuación " + nota.trim()
+                + " corresponde a la letra : Error - El número ingresado está fuera de rango válido.";
+
+        Assert.assertEquals(result, salidaCapturada);
+    }
+
+    @DisplayName("Prueba completa'")
+    @ParameterizedTest
+    @CsvFileSource(resources = "/csv/notes.csv")
+    void testMuestra(String respuesta, String nota) {
+
+        ByteArrayInputStream in = new ByteArrayInputStream(nota.getBytes());
+        System.setIn(in);
+        Main.sc = new Scanner(System.in);
+
+        Main.main(new String[1]);
+
+        String salidaCapturada = outputStream.toString().trim();
+
+        String result = "Ingrese una puntuación para calificar\n" + //
+                "La clasificación correspondiente a la puntuación " + nota.trim() + " corresponde a la letra "
+                + respuesta;
+
+        Assert.assertEquals(result, salidaCapturada);
+    }
+
 }
