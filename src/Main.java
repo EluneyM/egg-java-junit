2053af52f71dabd57f93e66f2b025bd611660d79
Matias <eluneymerillan@gmail.com>
2024-12-11 20:37:59 -0300

    REFACTOR quitando comentarios y acomodando scanner

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,37 @@
 package src;
+
+import java.util.Scanner;
+
 public class Main {
 
+    public static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Hola, Mundo!");
+        try (Scanner scanner = sc) {
+            System.out.println("Indica una posición para encontrar el número: ");
+            int num = scanner.nextInt();
+
+            System.out.println("El número en la secuencia es: " + fibonacciRecursivo(num));
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+    }
+
+    public static int fibonacciRecursivo(int num) {
+        int numSecuencia;
+        if (num < 2) {
+            if (num == 0) {
+                numSecuencia = 0;
+            } else {
+                numSecuencia = 1;
+            }
+            return numSecuencia;
+        } else {
+            numSecuencia = fibonacciRecursivo(num - 1) + fibonacciRecursivo(num - 2);
+            return numSecuencia;
+        }
+
     }
 }
