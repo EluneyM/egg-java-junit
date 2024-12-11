697db318b22f927761586eaac868c05ca5c46442
Matias <eluneymerillan@gmail.com>
2024-12-11 20:37:43 -0300

    Probando fibonacci

--- test/MainTest.java
+++ test/MainTest.java
@@ -1,13 +1,16 @@
 package test;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.Scanner;
 
 import org.junit.Assert;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import src.Main;
 
@@ -15,6 +18,7 @@ public class MainTest {
     private final PrintStream printStream = System.out;
 
     private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
 
     @BeforeEach
     public void setUp(){
@@ -26,14 +30,27 @@ public class MainTest {
         System.setOut(printStream);
     }
 
-    @Test
-    @DisplayName("Comprueba que main devulve el saludo")
-    void testMain() {
-        Main.main(new String[1]);
-        // Capturar la salida como una cadena
-        String salidaCapturada = outputStream.toString().trim(); // Usar trim() para eliminar saltos de línea al final
+    @ParameterizedTest(name = "[{index}] {arguments}")
+    @CsvSource(useHeadersInDisplayName = true, textBlock = """
+    POSICION,      VALOR_FIBONACCI
+    0,          0
+    1,          1
+    2,          1
+    3,          2
+    4,          3
+    """)
+    @DisplayName("Comprueba que fibonacci devuelve 0 en 0")
+    void testMain(int posicion, int valorFibonacci) {
+        byte[] entradaPorTeclado = (posicion+"\n").getBytes();
 
-        // Verificar que la salida sea la esperada
-        Assert.assertEquals("Hola, Mundo!", salidaCapturada);
+        ByteArrayInputStream in = new ByteArrayInputStream(entradaPorTeclado);
+        System.setIn(in);
+        Main.sc = new Scanner(System.in);
+
+        Main.main(new String[1]);
+
+        String[] salidaCapturada = outputStream.toString().trim().split("\n"); 
+
+        Assert.assertEquals("El número en la secuencia es: "+valorFibonacci, salidaCapturada[1]);
     }
 }
